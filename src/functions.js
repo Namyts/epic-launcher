5d2b60385163905235b72c52e4342a9402feda16
Namyts <35004248+Namyts@users.noreply.github.com>
2023-05-05 16:19:14 +0100

    beginning changes to make this work with ea app

--- src/functions.js
+++ src/functions.js
@@ -1,13 +1,10 @@
-import path, { dirname } from 'path'
-import { fileURLToPath } from 'url';
 import {exec} from 'child_process'
+import psList from 'ps-list';
 
 export const joinArgs = (args) => {
 	const quoteArgs = args.map(a=>a.includes(" ") ? `"${a}"` : a)
 	return quoteArgs.join(' ')
 }
-
-export const getThisDir = () => path.resolve(dirname(fileURLToPath(import.meta.url)),'..')
 
 export const execute = (command='cd',options={}) => new Promise((resolve,reject)=>{
 	const {verbose, logCommand, debug, ...rest} = options
@@ -24,3 +21,53 @@ export const execute = (command='cd',options={}) => new Promise((resolve,reject)
 })
 
 export const delay = (time) => new Promise(resolve=>setTimeout(resolve,time))
+
+export const close = err => {
+	console.error(err)
+	return (
+		delay(5000)
+		.then(()=>process.exit(1))
+	)
+}
+
+let startTime = new Date()
+let initialWait = 120
+let initialPollTime = 5
+let runningPollTime = 10
+let runningProcess = null
+
+export const waitForProcess = (name) => {
+	const message = runningProcess ? `[Checking]: ${name}` : `[Searching]: ${name}`
+	console.log(message)
+	return (
+		psList()
+		.then(processes=>{
+			const foundProcess = processes.find(p=>p.name === name)
+			if(foundProcess){
+				if(!runningProcess){ 
+					console.log('The game has been detected!')
+					runningProcess = foundProcess	
+				}
+				return (
+					delay(runningPollTime*1000)
+					.then(()=>waitForProcess(name))
+				)
+			} else {
+				if(runningProcess){
+					console.log('The game has been closed!')
+					process.exit()
+				} else {
+					if((new Date())-startTime > initialWait*1000){
+						return Promise.reject('Failed to launch the game :(')
+					} else {
+						return (
+							delay(initialPollTime*1000)
+							.then(()=>waitForProcess(name))
+						)
+					}
+				}
+			}
+			
+		})
+	)
+}
--- src/index.js
+++ src/index.js
@@ -1,90 +1,55 @@
 #!/usr/bin/env node --es-module-specifier-resolution=node
 
 import path from 'path'
-import psList from 'ps-list';
-import {execute, delay, joinArgs} from './functions'
 import commandLineArgs from 'command-line-args'
+import {close, execute, joinArgs, waitForProcess} from './functions.js'
 
 const argDefinitions = [
-	{ name: 'app', type: String, defaultOption: true },
-	{ name: 'dry-run', type: Boolean },
-	{ name: 'override-exe', alias: 'e', type: String },
+	{ name: 'debug', type: Boolean },
+	{ name: 'service', type: String, defaultOption: true },
+	{ name: 'epic-game', type: String },
+	{ name: 'epic-override-exe', type: String },
+	{ name: 'epic-other-args', type: String }
 ]
 const args = commandLineArgs(argDefinitions,{stopAtFirstUnknown: true})
 
 
 const DEBUG_MODE = args["dry-run"]
-const epicName = args.app
-const overrideExeArg = args["override-exe"]
-
+const service = args["service"]
 DEBUG_MODE && console.log(args)
 
-if(!epicName){
-	console.log('Add the epic games name as a command line argument')
-	process.exit(1)
-}
-
-let startTime = new Date()
-let initialWait = 120
-let initialPollTime = 5
-let runningPollTime = 10
-let runningProcess = null
-
-const waitForProcess = (name) => {
-	const message = runningProcess ? `[Checking]: ${name}` : `[Searching]: ${name}`
-	console.log(message)
-	return (
-		psList()
-		.then(processes=>{
-			const foundProcess = processes.find(p=>p.name === name)
-			if(foundProcess){
-				if(!runningProcess){ 
-					console.log('The game has been detected!')
-					runningProcess = foundProcess	
-				}
-				return (
-					delay(runningPollTime*1000)
-					.then(()=>waitForProcess(name))
-				)
-			} else {
-				if(runningProcess){
-					console.log('The game has been closed!')
-					process.exit()
+switch(service){
+	case "epic": {
+		const epicName = args["epic-game"]
+		const overrideExeArg = args["epic-override-exe"]
+		
+		if(!epicName){
+			console.log('Add the epic games name as a command line argument')
+			process.exit(1)
+		}
+		execute(`legendary list-installed --json`)
+			.then(res=>{
+				const installed_games = JSON.parse(res)
+				const game = installed_games.find(game=>game.app_name === epicName)
+				if(game){
+					DEBUG_MODE && console.log(game)
+					const exeName = overrideExeArg || game.executable
+					return (
+						execute(`legendary launch ${epicName} ${overrideExeArg?`--override-exe ${overrideExeArg}`:''}`,{verbose: true, debug: DEBUG_MODE})
+						.then(()=>DEBUG_MODE ? Promise.resolve() : waitForProcess(path.basename(exeName)))
+					)
 				} else {
-					if((new Date())-startTime > initialWait*1000){
-						return Promise.reject('Failed to launch the game :(')
-					} else {
-						return (
-							delay(initialPollTime*1000)
-							.then(()=>waitForProcess(name))
-						)
-					}
+					return Promise.reject(`${epicName} can't be found, or isn't installed...`)
 				}
-			}
-			
-		})
-	)
-}
-
-execute(`legendary list-installed --json`)
-.then(res=>{
-	const installed_games = JSON.parse(res)
-	const game = installed_games.find(game=>game.app_name === epicName)
-	if(game){
-		DEBUG_MODE && console.log(game)
-		const exeName = overrideExeArg || game.executable
-		return (
-			execute(`legendary launch ${joinArgs(process.argv.slice(2))}`,{verbose: true, debug: DEBUG_MODE})
-			.then(()=>DEBUG_MODE ? Promise.resolve() : waitForProcess(path.basename(exeName)))
-		)
-	} else {
-		return Promise.reject(`${epicName} can't be found, or isn't installed...`)
+			})
+			.catch(err=>close(err))
+		break;
 	}
-})
-.catch(err=>{
-	console.error(err)
-	return (
-		new Promise((resolve,reject)=>setTimeout(resolve,5000))
-		.then(()=>process.exit(1))
-	)
-})
+	case "ea": {
+		break;
+	}
+	default: {
+		close("No service has been selected...")
+		break;
+	}
+}
